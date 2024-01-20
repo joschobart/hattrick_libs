6b17c43a118b1ef4491af4848f185ba2d87d80ff
Joe <github@schicker.guru>
2024-01-20 16:41:24 +0100

    bugfixing get_matchdetails

--- ht_libs/get_matchdetails.py
+++ ht_libs/get_matchdetails.py
@@ -24,11 +24,17 @@ def get_matchdetails(matchdetails_xml):
 
     home_team_id, *_ = HomeTeam_tag.HomeTeamID.contents
     home_team_name, *_ = HomeTeam_tag.HomeTeamName.contents
-    home_team_goals, *_ = HomeTeam_tag.HomeGoals.contents
+    try:
+        home_team_goals, *_ = HomeTeam_tag.HomeGoals.contents
+    except Exception:
+        home_team_goals = ""
 
     away_team_id, *_ = AwayTeam_tag.AwayTeamID.contents
     away_team_name, *_ = AwayTeam_tag.AwayTeamName.contents
-    away_team_goals, *_ = AwayTeam_tag.AwayGoals.contents
+    try:
+        away_team_goals, *_ = AwayTeam_tag.AwayGoals.contents
+    except Exception:
+        away_team_goals = ""
 
     arena_id, *_ = Arena_tag.ArenaID.contents
     arena_name, *_ = Arena_tag.ArenaName.contents
