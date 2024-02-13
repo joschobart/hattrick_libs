6c2a8a7b109c6d0fd228afb663f95496cde83ee2
Joe <github@schicker.guru>
2024-02-13 17:27:15 +0100

    updated get_worlddetails

--- ht_libs/get_worlddetails.py
+++ ht_libs/get_worlddetails.py
@@ -12,15 +12,18 @@ def get_my_worlddetails(worlddetails_xml):
     world_soup = BeautifulSoup(worlddetails_xml, "xml")
 
     LeagueID_tag = world_soup.find("LeagueID")
+    LeagueName_tag = world_soup.find("LeagueName")
     NumberOfLevels_tag = world_soup.find("NumberOfLevels")
     MatchRound_tag = world_soup.find("MatchRound")
 
     league_id, *_ = LeagueID_tag.contents
+    league_name, *_ = LeagueName_tag.contents
     league_depth, *_ = NumberOfLevels_tag.contents
     season_round, *_ = MatchRound_tag.contents
 
     world_dict = {
         "league_id": league_id,
+        "league_name": league_name,
         "league_depth": league_depth,
         "season_round": season_round,
     }
